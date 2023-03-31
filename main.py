7f25583e2bcf46fbb9daa643fb7022cba0008765
Emil <emilhedy@gmail.com>
2023-03-31 22:28:08 +0200

    looking good

--- main.py
+++ main.py
@@ -1,10 +1,53 @@
+# Namn: Emil H
+# Datum 31/03/23
+# P-uppgift 193
+# Titel: Memory
+
 from time import time
 import random
 import tkinter as tk
+global player_name
 global attempt
 attempt = 0
 current_guesses = 0
 current_word = []
+
+    # NOT USED YET WILL BE IMPLEMENTED ONCE THE GAME WORKSSSSSSSS
+def info_menu():
+    info_menu = tk.Tk()
+    info_menu.title("info menu")
+
+    # beginning text
+    start_text = tk.Label(info_menu, text="Write size A, it will be of the size AxA, ONLY EVEN NUMBERS AND A < 20")
+    start_text.grid(row=0, column=0)
+
+    second_text = tk.Label(info_menu, text="player sick usrname :sunglass:")
+    second_text.grid(row=2, column=0)
+
+    input_size = tk.StringVar()
+    input_size_entry = tk.Entry(info_menu, textvariable=input_size)
+    input_size_entry.grid(row=1, column=0)
+
+    name_input = tk.StringVar()
+    name_input_entry = tk.Entry(info_menu, textvariable=name_input)
+    name_input_entry.grid(row=3, column=0)
+
+    def start_game():
+        try:
+            board_size = int(input_size_entry.get())
+            player_name = str(name_input_entry.get())
+            if board_size % 2 == 0:
+                info_menu.destroy()
+                global start_time
+                start_time = time()
+                return player_name
+        except ValueError:
+            print("Invalid input.")
+
+    start_game_button = tk.Button(info_menu, text="GO!!!!!!!!", command=start_game)
+    start_game_button.grid(row=4, column=0)
+    info_menu.mainloop()
+
 
 
 def create_game_list(file_name, sample_size):
@@ -30,24 +73,34 @@ def create_game_list(file_name, sample_size):
         random.shuffle(game_list)
         return game_list
 
-game_list = create_game_list("crewmatepack.txt", 18)
 
-def save_to_file(file_name, score):
-    with open(file_name, 'w') as f:
-        return
+game_list = create_game_list("memo.txt", 18)
+
+
+def save_to_file(file_name, username, score):
+    """Used to save the score of the user. Gonna have to exapnt this to check for the positions and then be able to shocase the file to the player.
+
+    Args:
+        file_name (str): the file name
+        score (int): the players score, highers = bad
+    """
+    with open(file_name, 'a') as f:
+        f.write(username + score + "\n")
         
 
-save_to_file("highscroe.txt", attempt)
+
 class Board:
     def __init__(self):
         """ Initialize an empty board """
         self.grid = {}
         rows = ['A', 'B', 'C', 'D', 'E', 'F']
         cols = [1, 2, 3, 4, 5, 6]
+
+        # self explaintory, fix somehow to be modular later, using ?
         for row in rows:
             for col in cols:
                 self.grid[row + str(col)] = None
-    
+
         self.matched_cards = []
 
     def set_value(self, row, col, value):
@@ -79,7 +132,7 @@ class Board:
             dict_values: A collection of all values in the grid.
         """
         return self.grid.values()
-    
+
     def hide_value(self, row, col):
         """Hide the value at the specified row and col on the grid.
 
@@ -97,7 +150,7 @@ class Board:
             col (int): The column number (1-6) of the matched card on the grid.
         """
         self.matched_cards.append(row + str(col))
-    
+
     def is_matched_card(self, row, col):
         """Check if the specified row and col represent a matched card.
 
@@ -109,14 +162,18 @@ class Board:
             bool: True if the card is matched, False otherwise.
         """
         return row + str(col) in self.matched_cards
-    
+
+
 # create a new grid object
 grid = Board()
+
 
 def add_words():
     """Goes through row and such and adds one word from the game list.
     """
     i = 0
+
+    # This adds words from the game_list with words from game_list
     for row in grid.grid.keys():
         grid.set_value(row[0], int(row[1]), game_list[i])
         if i < 36:
@@ -125,7 +182,9 @@ def add_words():
         else:
             break
 
+
 add_words()
+
 
 def reveal_word(row, col):
     """Reveal the word at the specified row and col on the grid.
@@ -136,19 +195,23 @@ def reveal_word(row, col):
     """
     # Get the corresponding button
     button = buttons[(row-1)*6 + (col-1)]
-    
+
     try:
         # Get the value of the corresponding card on the board
         global value, current_guesses
+
+        # the row + 64 exists to convert the row number to ASCII value, A is 65, chr() function convert ASCII value to the character itself.
+        # https://www.toppr.com/guides/python-guide/examples/python-examples/python-program-find-ascii-value-character/
+
         value = grid.get_value(chr(row + 64), col)
-        print("Value retrieved: {}".format(value))
+        print("Vword revealed : {}".format(value))
 
         # Set the text of the button to the revealed word
         button.config(text=value)
 
         # Disable the button so it cannot be clicked again
         button.config(state=tk.DISABLED)
-        button.config(bg = 'white', fg = 'black')
+        button.config(bg='white', fg='black')
         current_guesses += 1
         current_word.append([row, col])
 
@@ -156,8 +219,10 @@ def reveal_word(row, col):
         if current_guesses == 2:
             root.after(100, check_match)
             current_guesses = 0
+
     except Exception as e:
         print("Error: {}".format(str(e)))
+
 
 def hide_word(row, col):
     """Hide the word at the specified row and col on the grid.
@@ -166,14 +231,17 @@ def hide_word(row, col):
         row (int): The row number (1-6) of the card on the grid.
         col (int): The column number (1-6) of the card on the grid.
     """
+    # (row - 1) * 6: starting number of the row, will have to replace 6 with A to make modular
+    # (col - 1): Offset within the row, the column.
 
     button = buttons[(row-1)*6 + (col-1)]
-    print("hide_word function")
+    print("hide_word function we chilling")
     if not grid.is_matched_card(chr(row + 64), col):
-        button.config(text ="")
+        button.config(text="")
         button.config(state=tk.NORMAL)
     else:
         grid.matched_cards.remove(chr(row + 64) + str(col))
+
 
 def check_match():
     """Check if the currently revealed cards are a match.
@@ -182,17 +250,22 @@ def check_match():
         bool: True if there is a match, False otherwise.
     """
     global current_word, current_guesses
+
+    # Once the list of the current displayed words are equal to two, they get stored properly and then the words is aquired from the grid.
     if current_word and len(current_word) == 2:
         row1, col1 = current_word[0]
         row2, col2 = current_word[1]
+        print("checking the match..... pog")
         value1 = grid.get_value(chr(row1 + 64), col1)
         value2 = grid.get_value(chr(row2 + 64), col2)
 
+        # If word one matches word two the grid att the matched cards.
         if value1 == value2:
             print("match")
             grid.add_matched_cards(chr(row1 + 64), col1)
             grid.add_matched_cards(chr(row2 + 64), col2)
         else:
+            # The opposite of the aformentioned, but here's theres a delay to allow the user to see the words before they hide.
             print("Not Match")
             root.after(1000, hide_word, row1, col1)
             root.after(1000, hide_word, row2, col2)
@@ -201,19 +274,31 @@ def check_match():
     current_guesses = 0
     current_word.clear()
 
+
 def create_labels():
-    """Create labels for rows and columns."""
+    """Create labels for rows and columns. Idk why bg='black' makes like only the small part black but im to tired to care."""
+    print("creating the labesl")
     for i in range(6):
-        row_label = tk.Label(my_frame, text=chr(i + 65), font=("Helvetica", 20), height=1, width=2, bg='black', fg='white')
-        row_label.grid(row=i + 1, column=0, padx=(50, 0), pady=(0, 10), sticky=tk.W)
+        row_label = tk.Label(
+            my_frame, text=chr(i + 65), font=("Helvetica", 20),
+            height=1, width=2, bg='black', fg='white'
+        )
+        row_label.grid(
+            row=i + 1, column=0, padx=(50, 0), pady=(0, 10), sticky=tk.W
+        )
 
-        col_label = tk.Label(my_frame, text=str(i + 1), font=("Helvetica", 20), height=1, width=2, bg='black', fg='white')
-        col_label.grid(row=0, column=i + 1, padx=(0, 10), pady=(50, 0), sticky=tk.N)
+        col_label = tk.Label(
+            my_frame, text=str(i + 1), font=("Helvetica", 20),
+            height=1, width=2, bg='black', fg='white'
+        )
+        col_label.grid(
+            row=0, column=i + 1, padx=(0, 10), pady=(50, 0), sticky=tk.N
+        )
 
+# make this also a function later zzzzzz
 # Tkinter GUI
 root = tk.Tk()
-root.title("Emil A1-F6 IQ Spel!")
-root.geometry("1080x800")
+root.title("Memory Spel!")
 root.config(bg='black')
 
 my_frame = tk.Frame(root)
@@ -221,44 +306,51 @@ my_frame.pack(pady=10)
 
 create_labels()
 
-buttons = []
-for i in range(6):
-    for j in range(6):
-        button = tk.Button(my_frame, text='', font=("Helvetica", 20), height=3, width=6, fg = 'white')
-        button.grid(row=i + 1, column=j + 1)
-        buttons.append(button)
-
+# make this all a function later
+def create_buttons():
+    global buttons
+    buttons = []
+    print("creating buttons")
+    for i in range(6):
+        for j in range(6):
+            button = tk.Button(my_frame, text='', font=("Helvetica", 20), height=3, width=6, fg='white')
+            button.grid(row=i + 1, column=j + 1)
+            buttons.append(button)
+    return buttons
+    
+create_buttons()
 
 
 # create a label widget to describe the input box
-label = tk.Label(root, text="Enter your input cordinate (A1-F6) >")
-label.pack(side=tk.BOTTOM,pady=12)
+label = tk.Label(root, text="Enter your input coordinate (A1-F6) >")
+label.pack(side=tk.BOTTOM, pady=12)
 
 # create an Entry widget to input the data
 entry = tk.Entry(root, width=20, font=("Helvetica", 20))
 entry.pack(side=tk.BOTTOM)
 
-# a function to store the input as a variable :3
+# why are these here, im scared to move them
 row = ""
 col = ""
 input_value = ""
 
+
 def store_input():
     """
-    A function to get the user input and process it.
+    A function to get the user input and process it or something.
     In: Nothing
     Out: Nothing
     """
     global current_guesses, current_word
     input_value = entry.get()
-    #attempt += 1
+    # attempt += 1
     try:
         if len(input_value) == 2:
-                row = ord(input_value[0].upper()) - 64
-                col = int(input_value[1])
-                reveal_word(row, col)
-                print("Word revealed.")
-            
+            row = ord(input_value[0].upper()) - 64
+            col = int(input_value[1])
+            reveal_word(row, col)
+            print("Word revealed.")
+
     except Exception as e:
         print("error :(".format(str(e)))
     finally:
@@ -270,10 +362,13 @@ def clear_text_input():
     entry.delete(0, tk.END)
 
 
-# knapp för att använda store input som ska spara inputen.
-button = tk.Button(root, text="Submit cordinate", command=store_input)
+# Button to save the input.
+button = tk.Button(root, text="Submit coordinate", command=store_input)
 button.pack(side=tk.BOTTOM)
 
 
 # Start the Tkinter event loop
 root.mainloop()
+
+if __name__ == '__main__':
+    info_menu()
